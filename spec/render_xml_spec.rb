2c2f600654fc4b25688fe00f9dd514fd42e5d4ab
Mike Williams <mdub@dogbiscuit.org>
2010-11-16 17:49:23 +1100

    So that's how you use ActionView.  Huh.

--- spec/render_xml_spec.rb
+++ spec/render_xml_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
+require 'action_view'
 
 describe "fixtures" do
+
+  it "contains books" do
+    Books.all.should be_kind_of(Array)
+  end
+
+end
+
+TEMPLATE_PATH = File.join(File.dirname(__FILE__), "fixtures", "templates")
+
+describe "ActionPack" do
   
-    it "contains books" do
-      Books.all.should be_kind_of(Array)
-    end
-    
+  it "can render XML" do
+    @base = ActionView::Base.new(TEMPLATE_PATH, {})
+    @base.render(:inline => "<%= 100 + 23 %>", :type => :erb).should == "123"
+  end
+  
 end
